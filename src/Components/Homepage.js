643fafff9673a38fb9dd282e88cc692d13c00964
DylanGuidry <dylanguidry20@gmail.com>
2022-07-15 00:31:51 -0700

    new pres function

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -17,7 +17,7 @@ function Homepage() {
         <>
             <div class='searchBox'>
                 <button onClick={handleSumbmit} class='searchButton' type='submit'><img src='/Images/pokeball.gif' width='30px' /></button>
-                <input class='searchInput' value={searchInput} onChange={(e) => setsearchInput(e.target.value)} type='text' name='' placeholder='Search for a Pokemon...'></input>
+                <input class='searchInput' value={searchInput} onChange={(e) => setsearchInput(e.target.value)} type='text' name='' placeholder='Press the Poké Ball...'></input>
             </div>
         </>
     )
